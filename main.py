9d894a048acbf9b95d43e8fcba27df090b28d3f4
Vinicius <viniciusalvesdelimacosta1@gmail.com>
2025-09-02 14:38:48 -0300

    funcoes de ativacao terminadas

--- main.py
+++ main.py
@@ -31,6 +31,16 @@ def criar_plano_cartesiano():
     ax.grid(True, linestyle='--', alpha=0.5)
     return ax
 
+def criar_plano_cartesiano_completo():
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_xlim(-2, 2)
+    ax.set_ylim(-2, 2)
+    ax.axhline(0, color='black', linewidth=1)
+    ax.axvline(0, color='black', linewidth=1)
+    ax.grid(True, linestyle='--', alpha=0.5)
+    return ax
+
 def montar_plano_cartesiano(lista_resposta, y, cor, ax, label):
     coordenadasX = np.zeros(len(lista_resposta[0]))
     coordenadasY = np.zeros(len(lista_resposta[0]))
@@ -43,7 +53,7 @@ def montar_plano_cartesiano(lista_resposta, y, cor, ax, label):
 
     print("coordenadas Y")
     print(coordenadasY)
-    
+
     ax.plot(coordenadasX,coordenadasY,cor, label = label)
     ax.set_xlabel('V')
     ax.set_ylabel('Phi(V)')
@@ -128,6 +138,15 @@ def funcao_logistica(lista_resposta,amplificacao):
         y[i] = 1/(1+(numero_euler**((-amplificacao)*lista_resposta[2][i])))
     return y
 
+def funcao_tangente_hiperbolica(lista_resposta,amplificacao):
+    y = np.zeros(len(lista_resposta[0]))
+    for i in range(len(lista_resposta[0])):
+        #senh =((numero_euler**(amplificacao*lista_resposta[2][i]))-numero_euler**(-(amplificacao*lista_resposta[2][i])))/2
+        #cosh =((numero_euler**(amplificacao*lista_resposta[2][i]))+numero_euler**(-(amplificacao*lista_resposta[2][i])))/2
+        #y[i] = senh/cosh
+        y[i] = ((numero_euler**(2*(amplificacao*lista_resposta[2][i])))-1)/((numero_euler**(2*(amplificacao*lista_resposta[2][i])))+1)
+    return y
+
 
 
 
@@ -166,15 +185,21 @@ print(lista_referencia_com_resposta[2])
 plano_cartesiano_linear_por_partes = criar_plano_cartesiano()
 plano_cartesiano_limiar = criar_plano_cartesiano()
 plano_cartesiano_logistica = criar_plano_cartesiano()
+plano_cartesiano_tangente_hiperbolica= criar_plano_cartesiano_completo()
 
 plano_cartesiano_linear_por_partes.set_title("Linear Por Partes")
 plano_cartesiano_limiar.set_title("Limiar")
 plano_cartesiano_logistica.set_title("Logistica")
+plano_cartesiano_tangente_hiperbolica.set_title("Tangente Hiperbolica")
 
 y= funcao_de_linear_por_partes(lista_referencia_com_resposta,1)
 y_limiar = funcao_de_limiar(lista_referencia_com_resposta)
 y_logistica = funcao_logistica(lista_referencia_com_resposta,1)
 y_logistica_amplificacao10 = funcao_logistica(lista_referencia_com_resposta,10)
+y_tangente_hiperbolica = funcao_tangente_hiperbolica(lista_referencia_com_resposta,1)
+y_tangente_hiperbolica_amplificacao10  = funcao_tangente_hiperbolica(lista_referencia_com_resposta,10)
+
+
 print("Linear por partes")
 print(y)
 
@@ -186,6 +211,11 @@ print(y_logistica)
 print("Logistica A:10")
 print(y_logistica_amplificacao10)
 
+print("Tangente Hiperbolica A:1")
+print(y_tangente_hiperbolica)
+print("Tangente Hiperbolica A:10")
+print(y_tangente_hiperbolica_amplificacao10)
+
 
 plano_cartesiano_linear_por_partes = montar_plano_cartesiano(lista_referencia_com_resposta, y, 'go', plano_cartesiano_linear_por_partes, "Y")
 
@@ -194,6 +224,11 @@ plano_cartesiano_limiar = montar_plano_cartesiano(lista_referencia_com_resposta,
 plano_cartesiano_logistica= montar_plano_cartesiano(lista_referencia_com_resposta, y_logistica,"ro",plano_cartesiano_logistica,"A=1")
 plano_cartesiano_logistica= montar_plano_cartesiano(lista_referencia_com_resposta, y_logistica_amplificacao10,"co",plano_cartesiano_logistica,"A=10")
 plano_cartesiano_logistica.legend()
+
+plano_cartesiano_tangente_hiperbolica = montar_plano_cartesiano(lista_referencia_com_resposta, y_tangente_hiperbolica,"ro", plano_cartesiano_tangente_hiperbolica,"A=1")
+plano_cartesiano_tangente_hiperbolica = montar_plano_cartesiano(lista_referencia_com_resposta, y_tangente_hiperbolica_amplificacao10,"go", plano_cartesiano_tangente_hiperbolica,"A=10")
+plano_cartesiano_tangente_hiperbolica.legend()
+
 #funcao_de_limiar(lista_resposta)
 #funcao_de_linear_por_partes(lista_resposta, a=0.5)
 #potencial_ativacao(lista_resposta)
